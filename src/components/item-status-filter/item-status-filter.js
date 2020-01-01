2de535403ad1fb863017313972575739e8721759
serhii.shramko <shramko.web@yahoo.com>
2020-01-01 17:52:09 +0200

    refactor status-filter to more semantic

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
-    return (
-        <div className="btn-group btn-group-toggle" data-toggle="buttons">
-            <label className="btn btn-secondary active">
-                <input type="radio" name="options" id="filter-all" defaultChecked/> All
-            </label>
-            <label className="btn btn-secondary">
-                <input type="radio" name="options" id="filter-active"/> Active
-            </label>
-            <label className="btn btn-secondary">
-                <input type="radio" name="options" id="filter-done"/> Done
-            </label>
-        </div>
-    );
+class ItemStatusFilter extends PureComponent {
+    render() {
+        return (
+            <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                <label className="btn btn-secondary active">
+                    <input type="radio" name="options" id="filter-all" defaultChecked/> All
+                </label>
+                <label className="btn btn-secondary">
+                    <input type="radio" name="options" id="filter-active"/> Active
+                </label>
+                <label className="btn btn-secondary">
+                    <input type="radio" name="options" id="filter-done"/> Done
+                </label>
+            </div>
+        );
+    }
 };
 
 export default ItemStatusFilter;
