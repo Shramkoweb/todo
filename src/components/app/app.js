81d55848c698e5a8f70c5d5d893132f3d23e829d
serhii.shramko <shramko.web@yahoo.com>
2020-02-24 21:42:52 +0200

    refactor: rewrite app to hooks

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React, { useState } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -15,45 +15,29 @@ const mockTodoLabels = [
     'Buy a bread',
 ];
 
-class App extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.globaldId = 0;
-
-        this.state = {
-            todoData: mockTodoLabels.map((item, index) => {
-                return this._createTodoItem(item, index % 2)
-            }),
-            searchPhrase: '',
-            filter: 'all',
-        };
-
-        this._deleteTodoHandler = this._deleteTodoHandler.bind(this);
-        this._addTodoHandler = this._addTodoHandler.bind(this);
-        this._toggleTodoDoneStateHandler = this._toggleTodoDoneStateHandler.bind(this);
-        this._toggleTodoImportantStateHandler = this._toggleTodoImportantStateHandler.bind(this);
-        this._searchInputChange = this._searchInputChange.bind(this);
-    }
-
-    _createTodoItem(label, isImportant = false) {
+const App = () => {
+    const createTodoItem = (label, isImportant = false) => {
         return {
             done: false,
-            id: this.globaldId++,
+            id: globalID++,
             important: isImportant,
             label,
         };
-    }
+    };
+
+    let globalID = 0;
+
+    const mockTodos = mockTodoLabels.map((item, index) => createTodoItem(item, index % 2));
+    const [todoData, setTodoData] = useState(mockTodos);
+    const [searchPhrase, setSearchPhrase] = useState('');
+    const [filter, setFilter] = useState('all');
 
 
-    _deleteTodoHandler(id) {
-        this.setState(({todoData}) => {
-            return {
-                todoData: todoData.filter((item) => item.id !== id)
-            }
-        });
-    }
+    const deleteTodoHandler = (id) => {
+        setTodoData(todoData.filter((item) => item.id !== id))
+    };
 
-    _togglePropertyIn(array, id, propertyName) {
+    const togglePropertyIn = (array, id, propertyName) => {
         const todoIndex = array.findIndex((item) => item.id === id);
         const oldItem = array[todoIndex];
         const newItem = {...oldItem, [propertyName]: !oldItem[propertyName]};
@@ -62,55 +46,40 @@ class App extends PureComponent {
             newItem,
             ...array.slice(todoIndex + 1)
         ];
-    }
-
-    _toggleTodoDoneStateHandler(id) {
-        this.setState(({todoData}) => {
-            return {
-                todoData: this._togglePropertyIn(todoData, id, 'done')
-            };
-        });
-    }
-
-    _toggleTodoImportantStateHandler(id) {
-        this.setState(({todoData}) => {
-            return {
-                todoData: this._togglePropertyIn(todoData, id, 'important')
-            };
-        });
-    }
-
-    _addTodoHandler(text) {
-        this.setState(({todoData}) => {
-            const todoTemplate = this._createTodoItem(text, false);
-
-            return {
-                todoData: [
-                    ...todoData,
-                    todoTemplate
-                ]
-            };
-        });
-    }
-
-    _searchInputChange(searchPhrase) {
-        this.setState({searchPhrase});
-    }
-
-    onFilterChange = (filter) => {
-        this.setState({filter});
     };
 
-    searchTodoByTerm(initialState, searchPhrase) {
+    const toggleDoneState = (id) => {
+        setTodoData(togglePropertyIn(todoData, id, 'done'));
+    };
+
+    const toggleImportantState = (id) => {
+        setTodoData(togglePropertyIn(todoData, id, 'important'));
+    };
+
+    const addTodo = (text) => {
+        const todoTemplate = createTodoItem(text, false);
+
+        setTodoData([...todoData, todoTemplate]);
+    };
+
+    const onSearchChange = (searchPhrase) => {
+        setSearchPhrase(searchPhrase);
+    };
+
+    const onFilterChange = (filter) => {
+        setFilter(filter);
+    };
+
+    const searchTodoByTerm = (initialState, searchPhrase) => {
         if (searchPhrase === "") {
             return initialState;
         }
 
         return initialState.filter((item) => item.label.toLowerCase().indexOf(searchPhrase.toLowerCase()) > -1);
-    }
+    };
 
 
-    filterItems(items, filter) {
+    const filterTodos = (items, filter) => {
         if (filter === 'all') {
             return items;
         } else if (filter === 'active') {
@@ -118,39 +87,36 @@ class App extends PureComponent {
         } else if (filter === 'done') {
             return items.filter((item) => item.done);
         }
-    }
+    };
 
-    render() {
-        const {todoData, searchPhrase, filter} = this.state;
-        const visibleItems = this.searchTodoByTerm(this.filterItems(todoData, filter), searchPhrase);
-        const doneTodosAmount = todoData.filter((item) => item.done).length;
-        const todosAmount = todoData.length - doneTodosAmount;
+    const visibleItems = searchTodoByTerm(filterTodos(todoData, filter), searchPhrase);
+    const doneTodosAmount = todoData.filter((item) => item.done).length;
+    const todosAmount = todoData.length - doneTodosAmount;
 
-        return (
-            <div className="todo-app mx-auto mt-4">
-                <AppHeader toDo={todosAmount} done={doneTodosAmount}/>
-                <div className="d-flex mt-2 mb-2">
-                    <SearchPanel
-                        onSearchChange={this._searchInputChange}
-                    />
-                    <ItemStatusFilter
-                        filter={filter}
-                        onFilterChange={this.onFilterChange}
-                    />
-                </div>
-
-                <TodoList
-                    todos={visibleItems}
-                    onDeleteButtonClick={this._deleteTodoHandler}
-                    onToggleDone={this._toggleTodoDoneStateHandler}
-                    onToggleImportant={this._toggleTodoImportantStateHandler}
+    return (
+        <div className="todo-app mx-auto mt-4">
+            <AppHeader toDo={todosAmount} done={doneTodosAmount}/>
+            <div className="d-flex mt-2 mb-2">
+                <SearchPanel
+                    onSearchChange={onSearchChange}
                 />
-                <ItemAddForm
-                    onAddButtonClick={this._addTodoHandler}
+                <ItemStatusFilter
+                    filter={filter}
+                    onFilterChange={onFilterChange}
                 />
             </div>
-        );
-    }
+
+            <TodoList
+                todos={visibleItems}
+                onDeleteButtonClick={deleteTodoHandler}
+                onToggleDone={toggleDoneState}
+                onToggleImportant={toggleImportantState}
+            />
+            <ItemAddForm
+                onAddButtonClick={addTodo}
+            />
+        </div>
+    );
 };
 
 export default App;
